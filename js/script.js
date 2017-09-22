9d8a0bc5081540b6ca9dc349d4e3374988d62746
Kisseradjo <kisseradjo@gmail.com>
2017-09-22 12:57:46 -0400

    Add recent additions of the updated specs 'pingpong'

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ var pingPong = function(number) {
   return "ping";
 };
 var pingPong = function(number) {
-  if (number % 3 === 0) {
+  if ((number % 3 === 0) &&
+    (number % 5 !== 0)) {
     return "ping";
-  } else if (number % 5 === 0) {
+  } else if ((number % 5 === 0) && (number % 15 !== 0)) {
     return "pong";
+  } else {
+    return "pingpong";
   }
 };
 
