38871883e890d7ef97b38f4a77f24d00988b0adb
Kisseradjo <kisseradjo@gmail.com>
2017-09-30 11:50:51 -0400

    Add further development to js and delete picture panel

--- js/script.js
+++ js/script.js
@@ -2,14 +2,21 @@
 // var pingPong = function(number) {
 //   return "ping";
 // };
-var pingPong = function(number) {
-  if ((number % 3 === 0) &&
-    (number % 5 !== 0)) {
-    return "ping";
-  } else if ((number % 5 === 0) && (number % 15 !== 0)) {
-    return "pong";
-  } else if ((number % 15 === 0) && (number >= 15)) {
-    return "pingpong";
+var displayNumbers = [];
+var pingPong = function(number, displayNumbers) {
+  for (i = 1; i <= number; i++)
+
+  {
+    if ((number % 3 === 0) &&
+      (number % 5 !== 0)) {
+      displayNumbers.push("ping");
+    } else if ((number % 5 === 0) && (number % 15 !== 0)) {
+      displayNumbers.push("pong");
+    } else if ((number % 15 === 0) && (number >= 15)) {
+      displayNumbers.push("pingpong");
+    } else {
+      displayNumbers.push(number);
+    }
   }
 };
 // jQuery user interface logic
@@ -17,6 +24,10 @@ $(document).ready(function() {
   $("#click").click(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
+    pingPong(number, displayNumbers);
+    displayNumbers.forEach(function(output) {
+      $('#display').append("<li>" + output + "</li>");
+    });
     var result = pingPong(number);
     $("#result").text(result);
   });
